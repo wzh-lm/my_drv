110bff593d2313cd6ae6dc019edea9af4c4e27cc
wzh <531245204@qq.com>
2017-07-06 00:32:14 +0800

    clean some unused files

--- 2nd_term/.buttons_input.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/work/wzh/second_term/buttons_input.ko := arm-linux-ld -EL  -r -o /work/wzh/second_term/buttons_input.ko /work/wzh/second_term/buttons_input.o /work/wzh/second_term/buttons_input.mod.o
